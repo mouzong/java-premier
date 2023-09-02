602f70021a867f67de0a87fc762ca9671eadba30
Andreas MOUZONG <andreasmouzong@gmail.com>
2023-09-02 12:32:46 +0200

    Ajout des string

---
+++ src/andreas/strings/MainString.java
@@ -0,0 +1,30 @@
+package andreas.strings;
+
+import java.util.Scanner;
+
+public class MainString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        StringMethode.enregistrerClient(sc);
+//        String premiereMethodeA = "Premiere chaine de caractere";
+//        String premiereMethodeB = "Premiere Chaine De Caractere.";
+//
+//        System.out.println("\n\t Methode 1 :"+ premiereMethodeA == premiereMethodeB);
+//
+//        System.out.println("\n\t Caractere a l'indice 3 (position 4): "+premiereMethodeA.charAt(3));
+//        System.out.println("\n\t La longueur de la chaine :"+ premiereMethodeA.length());
+//        System.out.println("\n\t "+premiereMethodeA.equals(premiereMethodeB));
+//        System.out.println("\n\t "+premiereMethodeA.compareToIgnoreCase(premiereMethodeB));
+//        System.out.println("\n\t "+premiereMethodeA.toLowerCase());
+//        System.out.println("\n\t "+premiereMethodeA.toUpperCase());
+//
+//
+//
+//        String deuxiemeMethodeA = new String("Deuxieme chaine de caractere");
+//        String deuxiemeMethodeB = new String("Deuxieme chaine de caractere");
+//
+//        System.out.println("\n\t Methode 2 :"+deuxiemeMethodeA == deuxiemeMethodeB);
+    }
+}
