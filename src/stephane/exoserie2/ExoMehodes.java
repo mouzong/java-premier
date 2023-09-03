021b069a6e201a02f7755c65498b085975dbdd4d
Stephane YOT <merlinstephane9@gmail.com>
2023-09-03 11:12:56 +0200

    Ajout de l'exo palindrome

--- src/stephane/exoserie2/ExoMehodes.java
+++ src/stephane/exoserie2/ExoMehodes.java
@@ -30,4 +30,30 @@ public class ExoMehodes {
              System.out.println("Le nombre de voyelle est : "+ nbreVoyelle);
 
     }
+    public static void palindrome(Scanner sc){
+
+        System.out.println("\t\t ***** Vérification PALINDROME ***** ");
+              System.out.print("\t\t Entrez le mot à vérifier : ");
+            // Recuperation du mot entré au clavier
+
+            String mot = sc.nextLine();
+            boolean pal = true;
+            int i=0;
+           // Comparaison des caractères du mot afin de vérifier s'il est un palindrome
+          while (i < mot.length()){
+            if (mot.charAt(i) != mot.charAt(mot.length()-1-i)){
+                pal = false;
+                 break;
+            }
+//             pal = mot.charAt(i) != mot.charAt(mot.length()-1-i) ? false : true ;
+                 i++;
+          }
+            // Affichage du resultat
+               if (pal == true)
+                System.out.println("\t\t\t\t"+mot+" est un palindrome");
+             else System.out.println("\t\t\t\t"+mot +" n'est pas un palindrome");
+    }
+
+
 }
+
--- src/stephane/exoserie2/Serie2Main.java
+++ src/stephane/exoserie2/Serie2Main.java
@@ -6,6 +6,7 @@ public class Serie2Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        ExoMehodes.comptageVoyelles(sc);
+      //  ExoMehodes.comptageVoyelles(sc);
+        ExoMehodes.palindrome(sc);
     }
 }
